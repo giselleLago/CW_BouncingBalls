b6b1fa10f4a33d1de31318fa9c7659808f97e9d2
giselleLago <gisellelago25994@gmail.com>
2020-08-12 18:13:28 +0200

    Add project files.

---
+++ CW_BouncingBalls/BouncingBall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CW_BouncingBalls
+{
+    public class BouncingBall
+    {
+        public int bouncingBall(double h, double bounce, double window)
+        {
+            if (bounce >= 1 || bounce <= 0 || h <= 0 || window >= h)
+            {
+                return -1;
+            }
+            else
+            {
+                h *= bounce;
+                return 2 + bouncingBall(h, bounce, window);
+            }
+
+            /*if(h <= 0 || bounce <= 0 ||bounce >= 1 || window >= h) return -1;
+            int result = -1;
+  
+            do{
+              result+=2;
+              h*=bounce;
+            }while(h>window);
+  
+            return result;*/
+        }
+    }
+}
---
+++ CW_BouncingBalls/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_BouncingBalls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new BouncingBall();
+            Console.WriteLine(a.bouncingBall(30.0, 0.66, 1.5));
+            Console.ReadKey();
+        }
+    }
+}
